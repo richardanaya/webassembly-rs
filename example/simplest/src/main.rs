9c65fd1cf762d71be52d63725d00c3a6c3c9964c
Richard <Richard>
2020-04-05 06:05:08 -0700

    new function name

--- example/simplest/src/main.rs
+++ example/simplest/src/main.rs
@@ -15,12 +15,12 @@ fn main() -> std::io::Result<()> {
     {
         program.push(SECTION_TYPE); // start the section with it's type id
         let mut sec = vec![];
-        sec.extend_from_slice(&1u32.into_wasm_bytes()); // number of function signatures (only 1 for main)
+        sec.extend_from_slice(&1u32.to_wasm_bytes()); // number of function signatures (only 1 for main)
         sec.push(FUNC); // start of first function signature
-        sec.extend_from_slice(&0u32.into_wasm_bytes()); // first function signature has zero inputs
-        sec.extend_from_slice(&1u32.into_wasm_bytes()); // first function signature has one output
+        sec.extend_from_slice(&0u32.to_wasm_bytes()); // first function signature has zero inputs
+        sec.extend_from_slice(&1u32.to_wasm_bytes()); // first function signature has one output
         sec.push(I32); // first output is of type i32
-        program.extend_from_slice(&sec.len().into_wasm_bytes()); // push the byte length of the section
+        program.extend_from_slice(&sec.len().to_wasm_bytes()); // push the byte length of the section
         program.extend_from_slice(&sec); // push the data of the section
     }
 
@@ -28,9 +28,9 @@ fn main() -> std::io::Result<()> {
     {
         program.push(SECTION_FUNCTION);
         let mut sec = vec![];
-        sec.extend_from_slice(&1u32.into_wasm_bytes()); // number of functions (only 1 for main)
-        sec.extend_from_slice(&0u32.into_wasm_bytes()); // the first function should use the first signature we defined above
-        program.extend_from_slice(&sec.len().into_wasm_bytes()); // push the byte length of the section
+        sec.extend_from_slice(&1u32.to_wasm_bytes()); // number of functions (only 1 for main)
+        sec.extend_from_slice(&0u32.to_wasm_bytes()); // the first function should use the first signature we defined above
+        program.extend_from_slice(&sec.len().to_wasm_bytes()); // push the byte length of the section
         program.extend_from_slice(&sec); // push the data of the section
     }
 
@@ -38,11 +38,11 @@ fn main() -> std::io::Result<()> {
     {
         program.push(SECTION_MEMORY);
         let mut sec = vec![];
-        sec.extend_from_slice(&1u32.into_wasm_bytes()); // number of memory blocks (only 1 for this program)
+        sec.extend_from_slice(&1u32.to_wasm_bytes()); // number of memory blocks (only 1 for this program)
         sec.push(LIMIT_MIN_MAX); // first memory block defines min and max pages
-        sec.extend_from_slice(&2u32.into_wasm_bytes()); // first memory block min pages
-        sec.extend_from_slice(&10u32.into_wasm_bytes()); // first memory block max pages
-        program.extend_from_slice(&sec.len().into_wasm_bytes()); // push the byte length of the section
+        sec.extend_from_slice(&2u32.to_wasm_bytes()); // first memory block min pages
+        sec.extend_from_slice(&10u32.to_wasm_bytes()); // first memory block max pages
+        program.extend_from_slice(&sec.len().to_wasm_bytes()); // push the byte length of the section
         program.extend_from_slice(&sec); // push the data of the section
     }
 
@@ -50,21 +50,21 @@ fn main() -> std::io::Result<()> {
     {
         program.push(SECTION_EXPORT);
         let mut sec = vec![];
-        sec.extend_from_slice(&2u32.into_wasm_bytes()); // number of exports ( we have 1 function and 1 memory )
+        sec.extend_from_slice(&2u32.to_wasm_bytes()); // number of exports ( we have 1 function and 1 memory )
 
         let name = b"main";
-        sec.extend_from_slice(&name.len().into_wasm_bytes()); // first export name length
+        sec.extend_from_slice(&name.len().to_wasm_bytes()); // first export name length
         sec.extend_from_slice(name); // first export name data
         sec.push(DESC_FUNCTION); // first export is memory
-        sec.extend_from_slice(&0u32.into_wasm_bytes()); // first export is of the first function
+        sec.extend_from_slice(&0u32.to_wasm_bytes()); // first export is of the first function
 
         let name = b"memory";
-        sec.extend_from_slice(&name.len().into_wasm_bytes()); // second export name length
+        sec.extend_from_slice(&name.len().to_wasm_bytes()); // second export name length
         sec.extend_from_slice(name); // second export name data
         sec.push(DESC_MEMORY); // second export is memory
-        sec.extend_from_slice(&0u32.into_wasm_bytes()); // second export is of the first memory block
+        sec.extend_from_slice(&0u32.to_wasm_bytes()); // second export is of the first memory block
 
-        program.extend_from_slice(&sec.len().into_wasm_bytes()); // push the byte length of the section
+        program.extend_from_slice(&sec.len().to_wasm_bytes()); // push the byte length of the section
         program.extend_from_slice(&sec); // push the data of the section
     }
 
@@ -76,16 +76,16 @@ fn main() -> std::io::Result<()> {
         // code block for "main"
         let mut code = vec![];
         // we have zero local variables, if we did we'd specify their count and types here
-        code.extend_from_slice(&0u32.into_wasm_bytes());
+        code.extend_from_slice(&0u32.to_wasm_bytes());
         code.extend_from_slice(&[
             I32_CONST, 42,  // return 42
             END, // end function
         ]);
 
-        sec.extend_from_slice(&1.into_wasm_bytes()); // number of code blocks (only 1 for main)
-        sec.extend_from_slice(&code.len().into_wasm_bytes()); // length of first code block for "main"
+        sec.extend_from_slice(&1.to_wasm_bytes()); // number of code blocks (only 1 for main)
+        sec.extend_from_slice(&code.len().to_wasm_bytes()); // length of first code block for "main"
         sec.extend_from_slice(&code); // data of first code block for "main"
-        program.extend_from_slice(&sec.len().into_wasm_bytes()); // push the byte length of the section
+        program.extend_from_slice(&sec.len().to_wasm_bytes()); // push the byte length of the section
         program.extend_from_slice(&sec); // push the data of the section
     }
 
